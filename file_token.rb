74aeb4ab063975331cd4e025c552f466496b6998
Julian Martinez <julianeon@gmail.com>
2014-07-02 08:30:48 -0700

    Delete file_token.rb

--- file_token.rb
+++
@@ -1,9 +0,0 @@
-argument=ARGV[0]
-puts "ARGUMENT #{argument}"
-file=File.readlines(argument)
-file = file.map {|s| s.gsub(/CHANGE_THIS/,'CHANGE_THIS')}
-newfile=File.open(argument,'w')
-newfile.puts file
-newfile.close
-
-
